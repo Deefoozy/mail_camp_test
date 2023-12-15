f3ed18804a6215f62a1f802803d803766338502d
Deefoozy <tinotrok@outlook.com>
2023-12-15 19:29:51 +0100

    Added parsecontroller

---
+++ app/Http/Controllers/ParseController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ParsedMessage;
+
+class ParseController extends Controller
+{
+    // create parsing/encoding data table
+
+    public function parse() {
+        // Create validator
+
+        // parse input
+
+        // save input to database
+
+        // return 200 with parsed result
+    }
+
+    public function encode() {
+        // Create validator
+
+        // encode input to numbers
+
+        // save input to database
+
+        // return 200 with parsed result
+    }
+}
