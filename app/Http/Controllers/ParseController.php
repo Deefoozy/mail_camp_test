e3f7de20e4ce5e57ff17427c30e825bd077a11c8
Deefoozy <tinotrok@outlook.com>
2023-12-17 21:36:03 +0100

    Added requests and routes

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -5,41 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use App\Models\ParsedMessage;
+use App\Http\Requests\MessageParseRequest;
+use App\Http\Requests\MessageEncodeRequest;
 
 class ParseController extends Controller
 {
     // create parsing/encoding data table
 
-    public function parse() {
+    public function parse(MessageParseRequest $request) {
+        // $testMsg = "7777.666.555.33.2";
+
         // Create validator
+        $request->validated();
 
         // parse input
+        $message = $request->input('message');
 
         // save input to database
 
         // return 200 with parsed result
-
-        Response::json(
-            [
-                'message' => 'Hello world!',
-            ],
-            200
-        );
+        return response()
+            ->json([
+                'message' => $message,
+            ], 200);
     }
 
-    public function encode() {
-        // Create validator
+    public function encode(MessageEncodeRequest $request) {
+        // $testMsg = "hello world";
 
-        // encode input to numbers
+        // Create validator
+        $request->validated();
+
+        // encode input
+        $message = $request->input('message');
 
         // save input to database
 
         // return 200 with parsed result
-        Response::json(
-            [
-                'message' => 'Hello world!',
-            ],
-            200
-        );
+        return response()
+            ->json([
+                'message' => $message,
+            ], 200);
     }
 }
