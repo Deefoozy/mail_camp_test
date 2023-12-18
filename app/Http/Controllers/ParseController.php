68e7af86aa0fabbc0a4a6d37e2b6d7a6b64dfc25
Deefoozy <tinotrok@outlook.com>
2023-12-18 18:39:58 +0100

    Added parsing method

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -7,6 +7,99 @@ use Illuminate\Support\Facades\Response;
 use App\Models\ParsedMessage;
 use App\Http\Requests\MessageParseRequest;
 use App\Http\Requests\MessageEncodeRequest;
+use PhpParser\Node\Expr\Cast\String_;
+
+function parseMessage(string $input) {
+    // search for nokia keyboard functionality
+    // find correct file to put this in
+    $keys = [
+        '0' => [
+            'std' => [' '],
+            '*' => [],
+            '**' => []
+        ],
+        '1' => [
+            'std' => [''],
+            '*' => [],
+            '**' => []
+        ],
+        '2' => [
+            'std' => ['a', 'b', 'c'],
+            '*' => [],
+            '**' => []
+        ],
+        '3' => [
+            'std' => ['d', 'e', 'f'],
+            '*' => [],
+            '**' => []
+        ],
+        '4' => [
+            'std' => ['g', 'h', 'i'],
+            '*' => [],
+            '**' => []
+        ],
+        '5' => [
+            'std' => ['j', 'k', 'l'],
+            '*' => [],
+            '**' => []
+        ],
+        '6' => [
+            'std' => ['m', 'n', 'o'],
+            '*' => [],
+            '**' => []
+        ],
+        '7' => [
+            'std' => ['p', 'q', 'r', 's'],
+            '*' => [],
+            '**' => []
+        ],
+        '8' => [
+            'std' => ['t', 'u', 'v'],
+            '*' => [],
+            '**' => []
+        ],
+        '9' => [
+            'std' => ['w', 'x', 'y', 'z'],
+            '*' => [],
+            '**' => []
+        ],
+        '#' => [
+            'std' => [' '],
+            '*' => [],
+            '**' => []
+        ]
+    ];
+
+    $splitInputs = explode('.', $input);
+    $result = '';
+
+    foreach($splitInputs as $item) {
+        $indexedChar = '';
+        $indexedCharCount = 0;
+        $specialCount = 0;
+
+        foreach(mb_str_split($item) as $char) {
+            switch ($char) {
+                case '*':
+                    $specialCount++;
+                    break;
+                default:
+                    if (intval($char) >= 0) {
+                        $indexedChar = $char;
+                        $indexedCharCount++;
+                    }
+                    break;
+
+            }
+        };
+
+        if ($indexedCharCount > 0) {
+            $result .= $keys[$indexedChar]['std'][$indexedCharCount - 1];
+        }
+    };
+
+    return $result;
+}
 
 class ParseController extends Controller
 {
@@ -19,14 +112,16 @@ class ParseController extends Controller
         $request->validated();
 
         // parse input
-        $message = $request->input('message');
+        $inputMessage = $request->input('message');
+
+        $result = parseMessage($inputMessage);
 
         // save input to database
 
         // return 200 with parsed result
         return response()
             ->json([
-                'message' => $message,
+                'message' => $result,
             ], 200);
     }
 
