c6542e73937ffc13f7e6d508c882ad3e7c50ccee
Deefoozy <tinotrok@outlook.com>
2023-12-18 19:22:50 +0100

    Moved parsing method

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -9,98 +9,6 @@ use App\Http\Requests\MessageParseRequest;
 use App\Http\Requests\MessageEncodeRequest;
 use PhpParser\Node\Expr\Cast\String_;
 
-function parseMessage(string $input) {
-    // search for nokia keyboard functionality
-    // find correct file to put this in
-    $keys = [
-        '0' => [
-            'std' => [' '],
-            '*' => [],
-            '**' => []
-        ],
-        '1' => [
-            'std' => [''],
-            '*' => [],
-            '**' => []
-        ],
-        '2' => [
-            'std' => ['a', 'b', 'c'],
-            '*' => [],
-            '**' => []
-        ],
-        '3' => [
-            'std' => ['d', 'e', 'f'],
-            '*' => [],
-            '**' => []
-        ],
-        '4' => [
-            'std' => ['g', 'h', 'i'],
-            '*' => [],
-            '**' => []
-        ],
-        '5' => [
-            'std' => ['j', 'k', 'l'],
-            '*' => [],
-            '**' => []
-        ],
-        '6' => [
-            'std' => ['m', 'n', 'o'],
-            '*' => [],
-            '**' => []
-        ],
-        '7' => [
-            'std' => ['p', 'q', 'r', 's'],
-            '*' => [],
-            '**' => []
-        ],
-        '8' => [
-            'std' => ['t', 'u', 'v'],
-            '*' => [],
-            '**' => []
-        ],
-        '9' => [
-            'std' => ['w', 'x', 'y', 'z'],
-            '*' => [],
-            '**' => []
-        ],
-        '#' => [
-            'std' => [' '],
-            '*' => [],
-            '**' => []
-        ]
-    ];
-
-    $splitInputs = explode('.', $input);
-    $result = '';
-
-    foreach($splitInputs as $item) {
-        $indexedChar = '';
-        $indexedCharCount = 0;
-        $specialCount = 0;
-
-        foreach(mb_str_split($item) as $char) {
-            switch ($char) {
-                case '*':
-                    $specialCount++;
-                    break;
-                default:
-                    if (intval($char) >= 0) {
-                        $indexedChar = $char;
-                        $indexedCharCount++;
-                    }
-                    break;
-
-            }
-        };
-
-        if ($indexedCharCount > 0) {
-            $result .= $keys[$indexedChar]['std'][$indexedCharCount - 1];
-        }
-    };
-
-    return $result;
-}
-
 class ParseController extends Controller
 {
     // create parsing/encoding data table
@@ -114,14 +22,15 @@ class ParseController extends Controller
         // parse input
         $inputMessage = $request->input('message');
 
-        $result = parseMessage($inputMessage);
+        $parsedMessage = ParsedMessage::parseMessage($inputMessage);
 
         // save input to database
+        $parsedMessage->save();
 
         // return 200 with parsed result
         return response()
             ->json([
-                'message' => $result,
+                'message' => $parsedMessage->parsed_result,
             ], 200);
     }
 
--- app/Models/ParsedMessage.php
+++ app/Models/ParsedMessage.php
@@ -13,4 +13,99 @@ class ParsedMessage extends Model
         'parsed_result',
         'direction',
     ];
+
+    static function parseMessage(string $input) {
+        // search for nokia keyboard functionality
+        // find correct file to put this in
+        $keys = [
+            '0' => [
+                'std' => [' '],
+                '*' => [],
+                '**' => []
+            ],
+            '1' => [
+                'std' => [''],
+                '*' => [],
+                '**' => []
+            ],
+            '2' => [
+                'std' => ['a', 'b', 'c'],
+                '*' => [],
+                '**' => []
+            ],
+            '3' => [
+                'std' => ['d', 'e', 'f'],
+                '*' => [],
+                '**' => []
+            ],
+            '4' => [
+                'std' => ['g', 'h', 'i'],
+                '*' => [],
+                '**' => []
+            ],
+            '5' => [
+                'std' => ['j', 'k', 'l'],
+                '*' => [],
+                '**' => []
+            ],
+            '6' => [
+                'std' => ['m', 'n', 'o'],
+                '*' => [],
+                '**' => []
+            ],
+            '7' => [
+                'std' => ['p', 'q', 'r', 's'],
+                '*' => [],
+                '**' => []
+            ],
+            '8' => [
+                'std' => ['t', 'u', 'v'],
+                '*' => [],
+                '**' => []
+            ],
+            '9' => [
+                'std' => ['w', 'x', 'y', 'z'],
+                '*' => [],
+                '**' => []
+            ],
+            '#' => [
+                'std' => [' '],
+                '*' => [],
+                '**' => []
+            ]
+        ];
+
+        $splitInputs = explode('.', $input);
+        $result = '';
+
+        foreach($splitInputs as $item) {
+            $indexedChar = '';
+            $indexedCharCount = 0;
+            $specialCount = 0;
+
+            foreach(mb_str_split($item) as $char) {
+                switch ($char) {
+                    case '*':
+                        $specialCount++;
+                        break;
+                    default:
+                        if (intval($char) >= 0) {
+                            $indexedChar = $char;
+                            $indexedCharCount++;
+                        }
+                        break;
+    
+                }
+            };
+
+            if ($indexedCharCount > 0) {
+                $result .= $keys[$indexedChar]['std'][$indexedCharCount - 1];
+            }
+        };
+
+        return new ParsedMessage([
+            'parsed_result' => $result,
+            'direction' => 'raw_str',
+        ]);
+    }
 }
